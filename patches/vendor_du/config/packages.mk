773362e7eada9fe7fae5f0f9a032574ac8896f93
adeii <adeii@dr.com>
2016-10-10 11:20:46 +0200

    overwrite originals

---
+++ patches/vendor_du/config/packages.mk
@@ -0,0 +1,44 @@
+# Additional packages
+PRODUCT_PACKAGES += \
+	Development \
+	LatinIME \
+	VideoEditor \
+	VoiceDialer \
+	SoundRecorder \
+	Basic
+
+# Additional apps
+PRODUCT_PACKAGES += \
+	Apollo \
+	DashClock \
+	DSPManager \
+        DU-Changelog \
+	libcyanogen-dsp \
+	audio_effects.conf \
+        OmniSwitch \
+        Terminal \
+        SlimFileManager \
+        AndroidKernelTweaker
+
+PRODUCT_PACKAGES += \
+	CellBroadcastReceiver
+
+# Additional tools
+PRODUCT_PACKAGES += \
+	openvpn \
+	e2fsck \
+	mke2fs \
+	tune2fs \
+	bash \
+	vim \
+	nano \
+	htop \
+	powertop \
+	lsof \
+	mount.exfat \
+	fsck.exfat \
+	mkfs.exfat \
+        mkfs.f2fs \
+        fsck.f2fs \
+        fibmap.f2fs
+
